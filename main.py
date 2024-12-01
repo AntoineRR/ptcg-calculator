777033ad05c70b269f1a371cce219cccbb827e3b
AntoineRR <ant.romero2@orange.fr>
2024-12-01 14:38:34 +0100

    feat: add pull rates for other packs from genetic apex

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ def main():
         raw_pull_rates = json.load(file)
         pull_rates = PullRates.model_validate(raw_pull_rates)
     
-    print(pull_rates.get_rate("genetic_apex", "charizard", Rarity.CROWN))
+    rarity = Rarity.ONE_STAR
+    card_set = "genetic_apex"
+    pack = "mewtwo"
+
+    pull_rate = pull_rates.get_rate(card_set, pack, rarity)
+
+    print(f"Here are your chances of pulling a card of rarity {rarity.value} after opening one {pack} booster from {card_set}:")
+    print(f"{round(pull_rate, 2)}%")
+    print(f"On average, you need to open {round(100 / pull_rate, 2)} boosters to get one")
 
 
 if __name__ == "__main__":
